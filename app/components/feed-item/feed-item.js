15a18c68381169bf2b85c60f6948116bcd01ca57
Nathan <nazzanuk@gmail.com>
2017-07-26 13:45:54 +0100

    :)

--- app/components/feed-item/feed-item.js
+++ app/components/feed-item/feed-item.js
@@ -6,7 +6,7 @@ import * as _ from 'lodash';
 
 import * as ACTIONS from '../../constants/actions.constants';
 import {SessionList, isAudioAvailable} from '../../services/session.service';
-import {SetUrl, If} from '../../services/helper.service';
+import {SetUrl, If, IsDesktop, IsMobile, IsTablet} from '../../services/helper.service';
 import moment from "moment";
 
 require('./feed-item.scss');
@@ -28,14 +28,17 @@ class FeedItem extends React.Component {
             <div className="activity-box">
                 <div className="activity-image"></div>
 
-                <If condition={feedItem.feedAction = 'SIGNED_IN'}>
-                    <div className="activity-text">
-                        <strong className="linkable">{this.getUser(feedItem.user).name}</strong> signed in
-                        {/*<strong className="linkable"> Day 1 of Basics</strong>*/}
-                    </div>
-                </If>
 
-                <div className="activity-time"> &nbsp;- {moment(feedItem.time, 'YYYYMMDD-HH:mm:ss').fromNow()}</div>
+                <div className={`flex ${IsDesktop() ? 'flex-row' : ''}`}>
+                    <If condition={feedItem.feedAction = 'SIGNED_IN'}>
+                        <div className="activity-text">
+                            <strong className="linkable">{this.getUser(feedItem.user).name}</strong> signed in
+                            {/*<strong className="linkable"> Day 1 of Basics</strong>*/}
+                        </div>
+                    </If>
+
+                    <div className="activity-time"> &nbsp;- {moment(feedItem.time, 'YYYYMMDD-HH:mm:ss').fromNow()}</div>
+                </div>
                 <div style={{margin: 'auto'}}/>
                 <i className="activity-icon clickable likeable icon-heart"/>
                 <div className="activity-count">0</div>
