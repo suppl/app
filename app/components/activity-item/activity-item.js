5863ed6793c5a75b161c4e00a4fb78fca07d66b6
Nathan <nazzanuk@gmail.com>
2017-08-25 16:16:34 +0100

    :)

---
+++ app/components/activity-item/activity-item.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import {connect} from "react-redux";
+import {Link} from 'react-router-component';
+import Router from 'react-router-component'
+import * as _ from 'lodash';
+
+import * as ACTIONS from '../../constants/actions.constants';
+import * as FEED_ACTIONS from "../../constants/feed.constants";
+import {SessionList, isAudioAvailable} from '../../services/session.service';
+import {SetUrl, If, IsDesktop, IsMobile, IsTablet} from '../../services/helper.service';
+import moment from "moment";
+
+require('./activity-item.scss');
+
+
+class ActivityItem extends React.Component {
+    // const screen = this.props.screen;
+
+    getUser = (uid) => {
+        // console.info('getUser', uid, _.find(this.props.public.users, {uid}))
+        return _.find(this.props.public.users, {uid}) || {};
+    };
+
+    render() {
+
+        const feedItem = this.props.feedItem;
+
+        let color        = '#00A2F2';
+        let overlayWidth = '80%';
+        let name         = this.getUser(feedItem.user).name;
+        let text         = undefined;
+        let extra        = undefined;
+        let time         = moment(feedItem.time, 'YYYYMMDD-HH:mm:ss').fromNow();
+
+        let actions = {
+            [FEED_ACTIONS.SIGNED_IN]      : () => {
+                overlayWidth = '0%';
+                text         = <div>signed in</div>
+            },
+            [FEED_ACTIONS.STARTED_AUDIO]  : () => {
+                color        = _.find(SessionList, {id: feedItem.details.sessionId}).color;
+                overlayWidth = '80%';
+                text         = <div>
+                    started
+                    <Link className="activity-strong" href={`/sessions/${feedItem.details.sessionId}`}> {feedItem.details['sessionName']} - {feedItem.details.audioName}</Link>
+                </div>
+            },
+            [FEED_ACTIONS.COMPLETED_AUDIO]: () => {
+                color        = _.find(SessionList, {id: feedItem.details.sessionId}).color;
+                overlayWidth = '100%';
+                extra        = <div><img src="/statics/svg/icons/woohoo.svg" alt=""/> Woohoo!</div>;
+                text         = <div>
+                    completed
+                    <Link className="activity-strong" href={`/sessions/${feedItem.details.sessionId}`}> {feedItem.details['sessionName']} - {feedItem.details.audioName}</Link>
+                </div>
+            },
+        };
+
+        actions[feedItem.feedAction]();
+
+        return (
+            <div className="activity-panel">
+                <div className="activity-item">
+                    <div className="activity-header" style={{backgroundColor: color}}>
+                        <div className="header-overlay" style={{width: overlayWidth}}/>
+                    </div>
+
+                    <div className="activity-content">
+                        <div className="activity-icon"></div>
+                        <Link className="activity-user" href={`/profile/${feedItem.user}`}>{name}</Link>
+                        <div className="activity-text">{text}</div>
+                        <div className="activity-dash"/>
+                        <div className="activity-time">{time}</div>
+                        {extra ? <div>
+                            <div className="activity-dash"/>
+                            <div className="activity-extra">{extra}</div>
+                        </div> : ''}
+
+                    </div>
+
+
+                    {/*<div className={`flex ${IsDesktop() ? 'flex-row flex-align' : ''}`}>*/}
+                    {/*{(isAction(FEED_ACTIONS.SIGNED_IN)) ?*/}
+                    {/*<div className="activity-text">*/}
+                    {/*<strong className="linkable">{this.getUser(feedItem.user).name}</strong> signed in*/}
+                    {/*<strong className="linkable"> Day 1 of Basics</strong>*/}
+                    {/*</div> : ''*/}
+                    {/*}*/}
+                    {/*{(isAction(FEED_ACTIONS.STARTED_AUDIO)) ?*/}
+                    {/*<div className="activity-text">*/}
+                    {/*<strong className="linkable">{this.getUser(feedItem.user).name} </strong>*/}
+                    {/*started session*/}
+                    {/*<strong className="linkable">*/}
+                    {/*<Link href={`/sessions/${feedItem.details.sessionId}`}> {feedItem.details.sessionName} - {feedItem.details.audioName}</Link></strong>*/}
+                    {/*</div> : ''*/}
+                    {/*}*/}
+                    {/*{(isAction(FEED_ACTIONS.COMPLETED_AUDIO)) ?*/}
+                    {/*<div className="activity-text">*/}
+                    {/*<strong className="linkable">{this.getUser(feedItem.user).name} </strong>*/}
+                    {/*completed session*/}
+                    {/*<strong className="linkable">*/}
+                    {/*<Link href={`/sessions/${feedItem.details.sessionId}`}> {feedItem.details.sessionName} - {feedItem.details.audioName}</Link></strong>*/}
+                    {/*</div> : ''*/}
+                    {/*}*/}
+
+                    {/*<div className="activity-time"> &nbsp;- {moment(feedItem.time, 'YYYYMMDD-HH:mm:ss').fromNow()}</div>*/}
+                    {/*</div>*/}
+                    {/*<div style={{margin: 'auto'}}/>*/}
+                    {/*<i className="activity-icon clickable likeable icon-uniE7D4"/>*/}
+                    {/*<div className="activity-count">0</div>*/}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+const mapStateToProps = state => state;
+
+const mapDispatchToProps = dispatch => ({
+    showAudio: (session, audio) => dispatch({
+        type: ACTIONS.SHOW_AUDIO,
+        session,
+        audio
+    }),
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ActivityItem)
---
+++ app/screens-mobile/profile.screen.mobile.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import {connect} from "react-redux";
+import {Link} from 'react-router-component';
+import * as _ from 'lodash';
+
+import * as ACTIONS from '../constants/actions.constants';
+import HeaderMobile from '../components/header/header.mobile';
+import FooterMobile from '../components/footer/footer.mobile';
+import {Dispatch, State} from './../services/dispatch.service';
+import {SessionList, isAudioAvailable, isAudioDone} from '../services/session.service';
+import {CalcComplete, CalcStreak, CalcTotals, SetUrl} from '../services/helper.service';
+import ActivityItem from '../components/activity-item/activity-item';
+import Promo from '../components/promo/promo';
+
+
+class CommunityScreenMobile extends React.Component {
+    componentWillMount() {
+        setTimeout(() => {
+            this.activeClass = 'active-screen';
+            this.forceUpdate();
+        }, 1);
+    }
+
+    componentWillUpdate() {
+        $('.content-area').scrollTop(0);
+    }
+
+    render() {
+        let feed      = [];
+        let user      = this.props.public.user;
+        let profileId = this.props.profileId || this.props.public.user.uid;
+
+        const userRef = firebase.database().ref(`public/users/${profileId}`);
+        userRef.on('value', (snapshot) => {
+            user = snapshot.val();
+
+            const feedRef = firebase.database().ref(`feed/`).orderByChild('user').equalTo(user.uid).limitToLast(5);
+            feedRef.on('value', (snapshot) => feed = Object.values(snapshot.val()).reverse());
+        });
+
+        return (
+            <div data-screen className={`${this.activeClass}`}>
+                <div data-mobile-screen>
+
+                    <HeaderMobile/>
+                    <FooterMobile screen=""/>
+                    <div data-mobile-content style={{padding: 0}}>
+                        <div className="content-area" style={{paddingTop: 0}}>
+
+                            <div className="content-content">
+
+                                <div className="block light" style={{backgroundImage: `url('/statics/svg/hero/profile-hero.svg')`}}>
+                                    <div className="flex flex-row flex-between">
+                                        <div>
+                                            <div className="thin-heading">{user.name}</div>
+                                            <div className="thin-subheading">is feeling <strong>Excited</strong></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="thin-heading-2">{user.firstName || user.name}'s NEAT score</div>
+
+                                <div className="neat-banner">
+                                    <div className="neat-score">+{CalcTotals(user).NEAT}</div>
+                                    <div className="neat-text">Your <strong>NEAT</strong> score</div>
+                                </div>
+
+                                <div className="thin-heading-2">{user.firstName || user.name}'s performance</div>
+
+                                <div className="suppl-stat">
+                                    <img src="/statics/svg/dash/session-streak-icon.svg" className="stat-img"/>
+                                    <div className="flex flex-min">
+                                        <div className="stat-stat">
+                                            <span>{CalcStreak(user)}</span>
+                                            <span className="stat-small"> / day</span>
+                                        </div>
+                                        <div className="stat-text">Run streak</div>
+                                    </div>
+                                </div>
+                                <div className="suppl-stat">
+                                    <img src="/statics/svg/dash/session-complete-icon.svg" className="stat-img"/>
+                                    <div className="flex flex-min">
+                                        <div className="stat-stat">
+                                            <span>{CalcComplete(user)}</span>
+                                            <span className="stat-small"></span>
+                                        </div>
+                                        <div className="stat-text">Session(s) done</div>
+                                    </div>
+                                </div>
+                                <div className="suppl-stat">
+                                    <img src="/statics/svg/dash/posture-minute-icon.svg" className="stat-img"/>
+                                    <div className="flex flex-min">
+                                        <div className="stat-stat">
+                                            <span>{CalcTotals(user).durationMinutes}</span>
+                                            <span className="stat-small"> mins</span>
+                                        </div>
+                                        <div className="stat-text">Realign time</div>
+                                    </div>
+                                </div>
+
+                                <div className="thin-heading-2">{user.firstName || user.name}'s favourite sessions</div>
+
+                                <Promo size="mid" sessionId="standing" audioId="sat-at-work-01-01"/>
+                                <Promo size="mid" sessionId="back" audioId="sat-at-work-01-01"/>
+                                <Promo size="mid" sessionId="neck" audioId="sat-at-work-01-01"/>
+
+                                <div className="thin-heading-2">{user.firstName || user.name}'s latest activity</div>
+
+                                <div className="activity-holder">
+                                    {feed.map(feedItem => <ActivityItem feedItem={feedItem}/>)}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+const mapStateToProps = state => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (!user) SetUrl('/');
+    });
+
+    return state
+};
+
+const mapDispatchToProps = dispatch => ({
+
+    showAudio: (session, audio) => dispatch({
+        type: ACTIONS.SHOW_AUDIO,
+        session,
+        audio
+    }),
+
+    // showAward: (awardId) => dispatch({
+    //     type   : ACTIONS.SHOW_AWARD,
+    //     awardId: awardId
+    // }),
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CommunityScreenMobile)
